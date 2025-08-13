3c2d0fc22ee18905f9c1f3bd079e79db19ddbe52
MohammedAmr04 <mohammedamr200497@gmail.com>
2025-08-14 00:58:09 +0300

    refactor: update SidebarMinimal and SidebarMinimalItem components for improved navigation
    - Added URL handling in SidebarMinimalItem for navigation.
    - Cleaned up code formatting and structure in SidebarMinimal.
    - Enhanced item rendering to include URL prop for better routing.

--- components/layouts/sidebar/SidebarMinimal.tsx
+++ components/layouts/sidebar/SidebarMinimal.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import {
-   dashboardItems,
-   pagesItems,
-   settingsItems,
+  dashboardItems,
+  pagesItems,
+  settingsItems,
 } from "@/services/constants";
 import SidebarMinimalItem from "./dashboardSidebarComponents/SidebarMinimalItem";
 import { Dispatch, SetStateAction } from "react";
@@ -11,53 +11,56 @@ import { AlignJustify } from "lucide-react";
 import { useSidebar } from "@/services/context/SidebarContext";
 
 function SidebarMinimal({
-   isOpen,
-   setIsOpen,
+  isOpen,
+  setIsOpen,
 }: {
-   isOpen: boolean;
-   setIsOpen: Dispatch<SetStateAction<boolean>>;
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
 }) {
-   const { activeId, setActiveId } = useSidebar();
+  const { activeId, setActiveId } = useSidebar();
 
-   return (
-      <div className="min-w-21 flex-col flex py-6 items-center h-full">
-         <div>
-            <AlignJustify
-               width={24}
-               height={25}
-               className="cursor-pointer mb-6 bottom-0.5 relative "
-               onClick={() => setIsOpen(!isOpen)}
-            />
-         </div>
-
-         {dashboardItems.map((item) => (
-            <SidebarMinimalItem
-               key={item.id}
-               icon={item.icon}
-               onClick={() => {
-                  setActiveId(item.id);
-               }}
-               isActive={item.id === activeId}
-            />
-         ))}
-         {pagesItems.map((item) => (
-            <SidebarMinimalItem
-               key={item.id}
-               icon={item.icon}
-               onClick={() => setActiveId(item.id)}
-               isActive={item.id === activeId}
-            />
-         ))}
-         {settingsItems.map((item) => (
-            <SidebarMinimalItem
-               key={item.id}
-               icon={item.icon}
-               onClick={() => setActiveId(item.id)}
-               isActive={item.id === activeId}
-            />
-         ))}
+  return (
+    <div className="min-w-21 flex-col flex py-6 items-center h-full">
+      <div>
+        <AlignJustify
+          width={24}
+          height={25}
+          className="cursor-pointer mb-6 bottom-0.5 relative "
+          onClick={() => setIsOpen(!isOpen)}
+        />
       </div>
-   );
+
+      {dashboardItems.map((item) => (
+        <SidebarMinimalItem
+          url={item.url}
+          key={item.id}
+          icon={item.icon}
+          onClick={() => {
+            setActiveId(item.id);
+          }}
+          isActive={item.id === activeId}
+        />
+      ))}
+      {pagesItems.map((item) => (
+        <SidebarMinimalItem
+          url={item.url}
+          key={item.id}
+          icon={item.icon}
+          onClick={() => setActiveId(item.id)}
+          isActive={item.id === activeId}
+        />
+      ))}
+      {settingsItems.map((item) => (
+        <SidebarMinimalItem
+          url={item.url}
+          key={item.id}
+          icon={item.icon}
+          onClick={() => setActiveId(item.id)}
+          isActive={item.id === activeId}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default SidebarMinimal;
