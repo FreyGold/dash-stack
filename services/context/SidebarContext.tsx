4aecf352d823ad009a5d40fca7cb4df8531e5544
freygold <ahmedtawfik833@gmail.com>
2025-08-18 05:57:31 +0300

    fix: fix a bug where subroutes removed the highlight of sidebar tab

--- services/context/SidebarContext.tsx
+++ services/context/SidebarContext.tsx
@@ -1,44 +1,53 @@
 import { usePathname } from "next/navigation";
 import React, {
-  createContext,
-  useState,
-  useContext,
-  ReactNode,
-  useEffect,
+   createContext,
+   useState,
+   useContext,
+   ReactNode,
+   useEffect,
 } from "react";
 
 interface SidebarContextType {
-  activeId: string;
-  setActiveId: React.Dispatch<React.SetStateAction<string>>;
+   activeId: string;
+   setActiveId: React.Dispatch<React.SetStateAction<string>>;
 }
 
 const SidebarContext = createContext<SidebarContextType | undefined>(undefined);
 
 interface SidebarProviderProps {
-  children: ReactNode;
+   children: ReactNode;
 }
 
 export const SidebarProvider: React.FC<SidebarProviderProps> = ({
-  children,
+   children,
 }) => {
-  const pathname = usePathname();
-  const pathAfterLocale = pathname.replace(/^\/[^\/]+/, "");
-  const [activeId, setActiveId] = useState(() => pathAfterLocale.slice(1));
-  useEffect(() => {
-    setActiveId(pathAfterLocale.slice(1));
-  }, [pathAfterLocale]);
+   const pathname = usePathname();
+   let pathAfterLocale = pathname.replace(/^\/[^\/]+/, "");
+   if (pathAfterLocale.split("/").length > 3) {
+      console.log(1);
+      pathAfterLocale = pathAfterLocale.split("/").slice(1, 3).join("/");
+   } else {
+      console.log(2);
+      pathAfterLocale = pathAfterLocale.slice(1);
+   }
 
-  return (
-    <SidebarContext.Provider value={{ activeId, setActiveId }}>
-      {children}
-    </SidebarContext.Provider>
-  );
+   const [activeId, setActiveId] = useState(() => pathAfterLocale);
+
+   useEffect(() => {
+      setActiveId(pathAfterLocale);
+   }, [pathAfterLocale]);
+
+   return (
+      <SidebarContext.Provider value={{ activeId, setActiveId }}>
+         {children}
+      </SidebarContext.Provider>
+   );
 };
 
 export const useSidebar = () => {
-  const context = useContext(SidebarContext);
-  if (context === undefined) {
-    throw new Error("useSidebar must be used within a SidebarProvider");
-  }
-  return context;
+   const context = useContext(SidebarContext);
+   if (context === undefined) {
+      throw new Error("useSidebar must be used within a SidebarProvider");
+   }
+   return context;
 };
