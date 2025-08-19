106b7dfb774aa592303fd4f4ec08d9af916853e6
freygold <ahmedtawfik833@gmail.com>
2025-08-19 09:10:13 +0300

    refactor: improve notification handling and streamline save logic in FormEditFile component

--- components/dashboard/files/editfile/formEditFile/FormEditFile.tsx
+++ components/dashboard/files/editfile/formEditFile/FormEditFile.tsx
@@ -20,7 +20,32 @@ const FormEditFile = ({ file }: { file: FileData }) => {
    const [isEditingTag, setIsEditingTag] = useState(false);
    const [isEditingDescription, setIsEditingDescription] = useState(false);
 
-   // handle update
+   const [api, contextHolder] = notification.useNotification();
+
+   const showError = (message: string, description?: string) => {
+      api.error({
+         message,
+         description,
+         placement: "topRight",
+         duration: 4.5,
+      });
+   };
+   const showInfo = (message: string, description?: string) => {
+      api.info({
+         message,
+         description,
+         placement: "topRight",
+         duration: 4.5,
+      });
+   };
+   const showSuccess = (message: string) => {
+      api.success({
+         message,
+         placement: "topRight",
+         duration: 3,
+      });
+   };
+
    const handleSave = async (
       field: "destination_url" | "name" | "tag" | "description"
    ) => {
@@ -37,10 +62,11 @@ const FormEditFile = ({ file }: { file: FileData }) => {
 
       // إذا مفيش تغييرات، ما نرسلش الريكوست
       if (Object.keys(updateData).length === 0) {
-         notification.info({
-            message: "No changes to save",
-         });
-         return;
+         showInfo("No Changes To Save!");
+         if (field === "destination_url") setIsEditingDestination(false);
+         if (field === "name") setIsEditingName(false);
+         if (field === "tag") setIsEditingTag(false);
+         if (field === "description") setIsEditingDescription(false);
       }
 
       const { error } = await supabase
@@ -50,13 +76,13 @@ const FormEditFile = ({ file }: { file: FileData }) => {
 
       if (error) {
          console.error(error);
-         notification.error({
-            message: "Failed to update file",
-         });
+         showError("Failed to update file");
       } else {
-         notification.success({
-            message: "File updated successfully 🎉",
-         });
+         if (Object.keys(updateData).length == 0) {
+            return;
+         }
+         showSuccess("File updated successfully 🎉");
+
          if (field === "destination_url") setIsEditingDestination(false);
          if (field === "name") setIsEditingName(false);
          if (field === "tag") setIsEditingTag(false);
@@ -65,140 +91,143 @@ const FormEditFile = ({ file }: { file: FileData }) => {
    };
 
    return (
-      <div className="col-span-2 px-20 pt-7">
-         {/* Name */}
-         <div className="mb-8">
-            <div className="flex items-center justify-between">
-               <Typography.Title level={5}>Name</Typography.Title>
-               <PencilSimpleIcon
-                  className="!text-primary cursor-pointer"
-                  size={24}
-                  weight="bold"
-                  onClick={() => setIsEditingName(true)}
-               />
-            </div>
-            <Space.Compact className="w-full">
-               <Input
-                  size="large"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  disabled={!isEditingName}
-               />
-               {isEditingName && (
-                  <Button
+      <>
+         {contextHolder}
+         <div className="col-span-2 px-20 pt-7">
+            {/* Name */}
+            <div className="mb-8">
+               <div className="flex items-center justify-between">
+                  <Typography.Title level={5}>Name</Typography.Title>
+                  <PencilSimpleIcon
+                     className="!text-primary cursor-pointer"
+                     size={24}
+                     weight="bold"
+                     onClick={() => setIsEditingName(true)}
+                  />
+               </div>
+               <Space.Compact className="w-full">
+                  <Input
                      size="large"
-                     type="primary"
-                     onClick={() => handleSave("name")}>
-                     Save
-                  </Button>
-               )}
-            </Space.Compact>
-         </div>{" "}
-         {/* Description */}
-         <div className="mb-8">
-            <div className="flex items-center justify-between">
-               <Typography.Title level={5}>Description</Typography.Title>
-               <PencilSimpleIcon
-                  className="!text-primary cursor-pointer"
-                  size={24}
-                  weight="bold"
-                  onClick={() => setIsEditingDescription(true)}
-               />
-            </div>
-            <Space.Compact className="w-full">
-               <Input.TextArea
-                  size="large"
-                  value={description}
-                  onChange={(e) => setDescription(e.target.value)}
-                  disabled={!isEditingDescription}
-                  rows={4}
-               />
-               {isEditingDescription && (
-                  <Button
+                     value={name}
+                     onChange={(e) => setName(e.target.value)}
+                     disabled={!isEditingName}
+                  />
+                  {isEditingName && (
+                     <Button
+                        size="large"
+                        type="primary"
+                        onClick={() => handleSave("name")}>
+                        Save
+                     </Button>
+                  )}
+               </Space.Compact>
+            </div>{" "}
+            {/* Description */}
+            <div className="mb-8">
+               <div className="flex items-center justify-between">
+                  <Typography.Title level={5}>Description</Typography.Title>
+                  <PencilSimpleIcon
+                     className="!text-primary cursor-pointer"
+                     size={24}
+                     weight="bold"
+                     onClick={() => setIsEditingDescription(true)}
+                  />
+               </div>
+               <Space.Compact className="w-full">
+                  <Input.TextArea
                      size="large"
-                     type="primary"
-                     onClick={() => handleSave("description")}>
-                     Save
-                  </Button>
-               )}
-            </Space.Compact>
-         </div>
-         {/* Destination URL */}
-         <div className="mb-8">
-            <div className="flex items-center justify-between">
-               <Typography.Title level={5}>Destination URL</Typography.Title>
-               <PencilSimpleIcon
-                  className="!text-primary cursor-pointer"
-                  size={24}
-                  weight="bold"
-                  onClick={() => setIsEditingDestination(true)}
-               />
+                     value={description}
+                     onChange={(e) => setDescription(e.target.value)}
+                     disabled={!isEditingDescription}
+                     rows={4}
+                  />
+                  {isEditingDescription && (
+                     <Button
+                        size="large"
+                        type="primary"
+                        onClick={() => handleSave("description")}>
+                        Save
+                     </Button>
+                  )}
+               </Space.Compact>
             </div>
-            <Space.Compact className="w-full">
-               <Input
-                  size="large"
-                  value={destinationUrl}
-                  onChange={(e) => setDestinationUrl(e.target.value)}
-                  disabled={!isEditingDestination}
-               />
-               {isEditingDestination && (
-                  <Button
+            {/* Destination URL */}
+            <div className="mb-8">
+               <div className="flex items-center justify-between">
+                  <Typography.Title level={5}>Destination URL</Typography.Title>
+                  <PencilSimpleIcon
+                     className="!text-primary cursor-pointer"
+                     size={24}
+                     weight="bold"
+                     onClick={() => setIsEditingDestination(true)}
+                  />
+               </div>
+               <Space.Compact className="w-full">
+                  <Input
                      size="large"
-                     type="primary"
-                     onClick={() => handleSave("destination_url")}>
-                     Save
-                  </Button>
-               )}
-            </Space.Compact>
-         </div>
-         {/* ShortLink Section */}
-         <div className="mb-8">
-            <div className="flex items-center justify-between">
-               <Typography.Title level={5}>ShortLink</Typography.Title>
-               <PencilSimpleIcon
-                  className="!text-primary"
-                  size={24}
-                  weight="bold"
-               />
+                     value={destinationUrl}
+                     onChange={(e) => setDestinationUrl(e.target.value)}
+                     disabled={!isEditingDestination}
+                  />
+                  {isEditingDestination && (
+                     <Button
+                        size="large"
+                        type="primary"
+                        onClick={() => handleSave("destination_url")}>
+                        Save
+                     </Button>
+                  )}
+               </Space.Compact>
             </div>
-            <Space.Compact className="w-full">
-               <Select size="large" defaultValue="Short.sh" options={[]} />
-               <Input
-                  size="large"
-                  className="w-full"
-                  defaultValue="Link Shortening"
-               />
-            </Space.Compact>
-         </div>
-         {/* Tag */}
-         <div className="mb-8">
-            <div className="flex items-center justify-between">
-               <Typography.Title level={5}>Tag</Typography.Title>
-               <PencilSimpleIcon
-                  className="!text-primary cursor-pointer"
-                  size={24}
-                  weight="bold"
-                  onClick={() => setIsEditingTag(true)}
-               />
-            </div>
-            <Space.Compact className="w-full">
-               <Input
-                  size="large"
-                  value={tag}
-                  onChange={(e) => setTag(e.target.value)}
-                  disabled={!isEditingTag}
-               />
-               {isEditingTag && (
-                  <Button
+            {/* ShortLink Section */}
+            <div className="mb-8">
+               <div className="flex items-center justify-between">
+                  <Typography.Title level={5}>ShortLink</Typography.Title>
+                  <PencilSimpleIcon
+                     className="!text-primary"
+                     size={24}
+                     weight="bold"
+                  />
+               </div>
+               <Space.Compact className="w-full">
+                  <Select size="large" defaultValue="Short.sh" options={[]} />
+                  <Input
                      size="large"
-                     type="primary"
-                     onClick={() => handleSave("tag")}>
-                     Save
-                  </Button>
-               )}
-            </Space.Compact>
+                     className="w-full"
+                     defaultValue="Link Shortening"
+                  />
+               </Space.Compact>
+            </div>
+            {/* Tag */}
+            <div className="mb-8">
+               <div className="flex items-center justify-between">
+                  <Typography.Title level={5}>Tag</Typography.Title>
+                  <PencilSimpleIcon
+                     className="!text-primary cursor-pointer"
+                     size={24}
+                     weight="bold"
+                     onClick={() => setIsEditingTag(true)}
+                  />
+               </div>
+               <Space.Compact className="w-full">
+                  <Input
+                     size="large"
+                     value={tag}
+                     onChange={(e) => setTag(e.target.value)}
+                     disabled={!isEditingTag}
+                  />
+                  {isEditingTag && (
+                     <Button
+                        size="large"
+                        type="primary"
+                        onClick={() => handleSave("tag")}>
+                        Save
+                     </Button>
+                  )}
+               </Space.Compact>
+            </div>
          </div>
-      </div>
+      </>
    );
 };
 
